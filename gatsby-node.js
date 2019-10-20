31acb390854aef2ee27fc3dc3554933d33a9b63f
Simrandeep <simrank.2210@gmail.com>
2019-10-20 21:17:58 +0100

    fix gatsby build

--- gatsby-node.js
+++ gatsby-node.js
@@ -57,7 +57,6 @@ exports.createPages = ({ graphql, actions }) => {
                     slug
                     langKey
                     directoryName
-                    maybeAbsoluteLinks
                   }
                   frontmatter {
                     title
@@ -138,26 +137,6 @@ exports.createPages = ({ graphql, actions }) => {
             // Record which links to internal posts have translated versions
             // into this language. We'll replace them before rendering HTML.
             let translatedLinks = [];
-            const { langKey, maybeAbsoluteLinks } = post.node.fields;
-            maybeAbsoluteLinks.forEach(link => {
-              if (allSlugs.has(link)) {
-                if (allSlugs.has('/' + langKey + link)) {
-                  // This is legit an internal post link,
-                  // and it has been already translated.
-                  translatedLinks.push(link);
-                } else if (link.startsWith('/' + langKey + '/')) {
-                  console.log('-----------------');
-                  console.error(
-                    `It looks like "${langKey}" translation of "${
-                      post.node.frontmatter.title
-                    }" ` +
-                      `is linking to a translated link: ${link}. Don't do this. Use the original link. ` +
-                      `The blog post renderer will automatically use a translation if it is available.`
-                  );
-                  console.log('-----------------');
-                }
-              }
-            });
 
             createPage({
               path: post.node.fields.slug,
